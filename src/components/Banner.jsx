ce9f1bdcf5eceaa5bd4b27346e68be550e7583ee
Vishal <vishaljaiswal224@gmail.com>
2023-12-12 00:49:42 +0530

    All Components Completed with css

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,6 +4,8 @@ import { ArrowRightCircle } from "react-bootstrap-icons";
 import headerImg from "../assets/img/header-img.svg";
 import { useState, useEffect } from "react";
 import "./Banner.css";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
 const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(0);
@@ -47,21 +49,31 @@ const Banner = () => {
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my portfolio</span>
-            <h1>
-              {`Hi there! I'm webdecoded `}
-              <span className="wrap" style={{ color: "#825a82" }}>
-                {text}
-              </span>
-            </h1>
-            <p>
-              Passionate front-end developer with a love for new technologies
-              and emerging trends. Always looking for new ways to build
-              interactive user interfaces.
-            </p>
-            <button onClick={() => console.log("connect")}>
-              Let's Connect <ArrowRightCircle size={25} />
-            </button>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animated__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my portfolio</span>
+                  <h1>
+                    {`Hi there! I'm webdecoded `}
+                    <span className="wrap" style={{ color: "#825a82" }}>
+                      {text}
+                    </span>
+                  </h1>
+                  <p>
+                    Passionate front-end developer with a love for new
+                    technologies and emerging trends. Always looking for new
+                    ways to build interactive user interfaces.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let's Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <img src={headerImg} alt="Header Img" />
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Col } from "react-bootstrap";
-const ProjectCard = ({ title, description, imgUrl }) => {
+const ProjectCard = ({ title, description, onClick, imgUrl }) => {
   return (
-    <Col sm={6} md={4}>
+    <Col
+      sm={6}
+      md={4}
+      onClick={() => {
+        if (title === "Movix-Appliation") {
+          window.open("https://movie-application-theta.vercel.app/", "_blank");
+        } else {
+          window.open("https://you-tube-clone-brown.vercel.app/", "_blank");
+        }
+      }}
+    >
       <div className="proj-imgbx">
         <img src={imgUrl} alt="" />
         <div className="proj-txtx">
